0c165c31b23357ac0cb605d869d5654dfc1d9628
mm <mm@matterlabs.dev>
2024-12-21 20:07:35 +0100

    experiments

--- src/Counter.sol
+++ src/Counter.sol
@@ -11,4 +11,34 @@ contract Counter {
     function increment() public {
         number++;
     }
+    function decimals() external view returns (uint8) {
+        return 18;
+    }
+
+    function latestAnswer() external view returns (int256) {
+        return 1000000000000000000;
+    }
+
+    function latestTimestamp() external view returns (uint256) {
+        return block.timestamp;
+    }
+
+    function latestRound() external view returns (uint256) {
+        return block.timestamp;
+    }
+
+    function getAnswer(uint256 roundId) external view returns (int256) {
+        return 1000000000000000000;
+    }
+
+    function getTimestamp(uint256 roundId) external view returns (uint256) {
+        return roundId;
+    }
+
+    event AnswerUpdated(
+        int256 indexed current,
+        uint256 indexed roundId,
+        uint256 timestamp
+    );
+    event NewRound(uint256 indexed roundId, address indexed startedBy);
 }
